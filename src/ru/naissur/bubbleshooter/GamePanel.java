c49df4deb59f8c674d6fe0d9ceef7dac058daf75
YmraNaissur <daily.spb@yandex.ru>
2017-07-11 15:43:55 +0300

    Нарисовал панель

--- src/ru/naissur/bubbleshooter/GamePanel.java
+++ src/ru/naissur/bubbleshooter/GamePanel.java
@@ -1,6 +1,7 @@
 package ru.naissur.bubbleshooter;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * naissur
@@ -8,4 +9,15 @@ import javax.swing.*;
  */
 public class GamePanel extends JPanel {
 
+    // Игровое поле
+    public static final int WIDTH = 400;    // ширина
+    public static final int HEIGHT = 400;   // высота
+
+    // Конструктор
+    public GamePanel() {
+        super();
+        setPreferredSize(new Dimension(WIDTH, HEIGHT)); // установили размеры панели
+        setFocusable(true);
+        requestFocus();
+    }
 }
